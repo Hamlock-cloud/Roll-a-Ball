c6122b48879c13a5f69743c74d61aff0424b3cd0
Hamlock-cloud <76624863+Hamlock-cloud@users.noreply.github.com>
2020-12-30 08:24:19 -0800

    Add files via upload

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winText;
+
+    private int count;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.SetActive(false);
+    }
+
+    // Update is called once per frame
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVactor = movementValue.Get<Vector2>();
+
+        movementX = movementVactor.x;
+        movementY = movementVactor.y;
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.SetActive(true);
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        
+        rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+}
